8b53f9093c3b6e865da1f233a4ea46d9ca055146
Gabriel Dragoni <ga.dragoni@gmail.com>
2019-10-03 10:51:00 -0300

    Initial commit

---
+++ Finances/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Finances
+//
+//  Created by Gabriel A. Dragoni on 03/10/19.
+//  Copyright © 2019 Dragoni. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
